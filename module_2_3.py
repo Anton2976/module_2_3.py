fb087b1b305d42d41d0f3bd14e79445467e26adb
Anton <ant-sungurov@yandex.ru>
2024-08-12 09:10:32 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,8 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+meaning = 0
+while (meaning < len(my_list)) and (my_list[meaning]) >= 0:
+    if my_list[meaning] < 0:
+        break
+    if my_list[meaning] > 0:
+        print(my_list[meaning])
+    meaning += 1
